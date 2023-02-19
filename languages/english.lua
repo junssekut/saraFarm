2caa969784956040e8df41b3d8ccf8aeee1cd518
junssekut <arjuna.andio@binus.ac.id>
2023-02-19 23:23:21 +0800

    Added english language.

---
+++ languages/english.lua
@@ -0,0 +1,29 @@
+return {
+    Locale = 'en',
+    Name = 'English',
+    Version = '1.0',
+    Author = 'junssekut#4964',
+    Webhook = {
+        ROTATION_LIMIT = 'Rotation Limit [x%d]',
+
+        STORING_PACK = 'Storing Pack [x%d %s]',
+        STORING_SEED = 'Storing Seed [x%d %s]',
+        STORING_ITEMS = 'Storing Specific Items',
+
+        FARM_INITIALIZING = 'Initializing Farm [%s]',
+        FARM_SKIPPING = 'Skipping Farm [%s]',
+        FARM_ROTATING = 'Rotating Farm [%s]',
+        FARM_RANDOMING_WOLRD = 'Random World [%s]',
+
+        RESTING = 'Resting [%d minutes left]',
+        RESTED = 'Rested Well',
+
+        PUT_AND_BREAK = 'Put And Break [x%d %s]',
+
+        CONNECTION_RECONNECTING = 'Reconnecting...',
+        CONNECTION_RECONNECTED = 'Reconnected [%s]',
+        CONNECTION_LIMIT = 'Connection Limit [x%d, %d minutes wait]',
+
+        CAPTCHA_FAILED = 'Captcha Failed'
+    }
+}
